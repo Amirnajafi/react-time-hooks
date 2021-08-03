725583a45e4848b9bda5f7f55b7d3e977618c0bf
amirnajafi <amir.najafi66@gmail.com>
2021-08-03 13:16:08 +0430

    fixing index bug

---
+++ dist/useTimer.js
@@ -0,0 +1,68 @@
+import { useEffect, useState } from 'react';
+
+const useTimer = (time = 0, options = {}) => {
+  const {
+    countOnFocus = false,
+    interval = 1,
+    autoStart = true,
+    stop_time = null
+  } = options;
+  const [state, setState] = useState({
+    time: time,
+    interval: null
+  });
+
+  const start_counter = () => {
+    state.interval = setInterval(() => {
+      if (stop_time && stop_time === state.time) {
+        return;
+      }
+
+      if (countOnFocus) {
+        if (document.hasFocus()) {
+          state.time = state.time + interval;
+          setState({ ...state
+          });
+        }
+      } else {
+        state.time = state.time + interval;
+        setState({ ...state
+        });
+      }
+    }, interval * 1000);
+  };
+
+  const reset = () => {
+    clearInterval(state.interval);
+    state.time = time;
+    setState({ ...state
+    });
+    play();
+  };
+
+  const puase = () => {
+    clearInterval(state.interval);
+  };
+
+  const play = () => {
+    start_counter();
+  };
+
+  useEffect(() => {
+    if (autoStart) {
+      play();
+    }
+
+    return () => {
+      clearInterval(state.interval);
+    };
+  }, []);
+  return {
+    time: state.time,
+    play,
+    puase,
+    reset
+  };
+};
+
+export default useTimer;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,7 @@
-// import useCountDown from './useCountDown'
-// import useTimer from './useTimer'
+import useCountDown from './useCountDown'
+import useTimer from './useTimer'
 
-// export {
-// 	useCountDown,
-// 	useTimer
-// }
-
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-	value: true
-});
-Object.defineProperty(exports, "useCountDown", {
-	enumerable: true,
-	get: function get() {
-		return _useCountDown["default"];
-	}
-});
-Object.defineProperty(exports, "useTimer", {
-	enumerable: true,
-	get: function get() {
-		return _useTimer["default"];
-	}
-});
-
-var _useCountDown = _interopRequireDefault(require("./useCountDown"));
-
-var _useTimer = _interopRequireDefault(require("./useTimer"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+export {
+	useCountDown,
+	useTimer
+}
