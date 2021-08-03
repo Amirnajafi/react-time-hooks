425628757fbe8656c5aae499d7e337eae504b12a
amirnajafi <amir.najafi66@gmail.com>
2021-08-03 10:56:58 +0430

    fix bugs

---
+++ dist/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "useCountDown", {
+  enumerable: true,
+  get: function get() {
+    return _useCountDown["default"];
+  }
+});
+Object.defineProperty(exports, "useTimer", {
+  enumerable: true,
+  get: function get() {
+    return _useTimer["default"];
+  }
+});
+
+var _useCountDown = _interopRequireDefault(require("./useCountDown"));
+
+var _useTimer = _interopRequireDefault(require("./useTimer"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
