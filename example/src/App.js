5ae3a4dd7afc19bc24e46d238f052a6dcdd6ef53
amirnajafi <amir.najafi66@gmail.com>
2021-08-03 17:26:39 +0430

    init commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,31 @@
 import React from 'react'
-
-import { ExampleComponent } from 'react-time-hooks'
+import { useCountDown, useTimer } from 'react-time-hooks'
 import 'react-time-hooks/dist/index.css'
-
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const { time: count_time, reset: count_reset, play: count_play, puase: count_puase } = useCountDown(60)
+  const { time, reset, play, puase } = useTimer(0, {
+    interval: 1,
+    countOnFocus: true
+  })
+  return (
+    <div className='flex flex-1 items-center justify-center flex-column mt-20'>
+      <h2 className='font-bold text-xl'>useTimer</h2>
+      <span>{time}</span>
+      <div className='flex space-x-2'>
+
+        <button onClick={play}>play</button>
+        <button onClick={puase}>pause</button>
+        <button onClick={reset}>reset</button>
+      </div>
+      <h2 className='font-bold text-xl'>useCountDown</h2>
+      <span>{count_time}</span>
+      <div className='flex space-x-2'>
+        <button onClick={count_play}>play</button>
+        <button onClick={count_puase}>pause</button>
+        <button onClick={count_reset}>reset</button>
+      </div>
+    </div>
+  )
 }
 
 export default App
